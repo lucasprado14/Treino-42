9eefdebc269cd55663bf1880e5767148281e02f2
lucasprado14 <joselucas_2004@hotmail.com>
2026-09-22 12:02:34 -0300

    Atualização exercício 01

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -1,18 +1,20 @@
 #include <unistd.h>
 
-void ft_print_alphabet(void)
+void	ft_print_alphabet(void)
 {
     char c;
-    
+
     c = 'a';
     while (c <= 'z')
-    {        write(1, &c, 1);
-        c++;     
-    }   
+    {
+        write(1, &c, 1);
+        c++;
+    }
+    
 }
 
 int main(void)
 {
     ft_print_alphabet();
-    return 0;
+    return (0);
 }
