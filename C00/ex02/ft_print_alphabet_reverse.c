3b6a726b94802b36b0c6e0f63b6cb9809e9c92e7
lucasprado14 <joselucas_2004@hotmail.com>
2026-09-22 12:41:46 -0300

    Exercicios 02 e 03 refeitos

--- C00/ex02/ft_print_alphabet_reverse.c
+++ C00/ex02/ft_print_alphabet_reverse.c
@@ -1,17 +1,21 @@
 #include <unistd.h>
-void ft_print_alphabet_reverse(void)
+
+void    ft_print_alphabet_reverse(void)
 {
     char c;
-    
-    c = 'z';
-    while (c >= 'a')
-    {        write(1, &c, 1);
-        c--;     
-    }   
-}   
 
-int main(void)
+    c = 'z';
+
+    while (c >= 'a')
+    {
+        write(1, &c, 1);
+        c--;
+    }
+    
+}
+
+int main (void)
 {
     ft_print_alphabet_reverse();
-    return 0;
-}   
+    return(0);
+}
