3d4e4ede1430fd44eac14c450c46e3041fb2c402
lucasprado14 <joselucas_2004@hotmail.com>
2026-09-22 18:03:34 -0300

    Atualização exercício 04

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -1,27 +1,20 @@
-#include <unistd.h> 
+#include <unistd.h>
 
-void ft_is_negative(int n) 
-{ 
-    char c; 
-
-    if (n < 0) 
-        c = 'N'; 
-    else 
-        c = 'P'; 
-
-    write(1, &c, 1); 
+void	ft_is_negative(int n)
+{
+    if (n >= 0)
+        write(1, "P", 1);
+    else
+        write(1, "N", 1);
 }
 
-int main(void) 
-{ 
-    ft_is_negative(-5); 
+int	main(void)
+{
+    ft_is_negative(5);
     write(1, "\n", 1);
 
-    ft_is_negative(0); 
+    ft_is_negative(-5);
     write(1, "\n", 1);
 
-    ft_is_negative(10); 
-    write(1, "\n", 1);
-    
-    return 0; 
+    return (0);
 }
